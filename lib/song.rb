bebff16ea9d124753a6bc19ad778845892694f18
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-09-04 12:05:02 +0300

    commit tree 2081e5f6b92ce60d0e7131dd8e985779fd93eb35

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,20 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all    
+    DB[:conn].execute("SELECT * FROM songs").map { |row| self.new_from_db(row) } 
+  end
+
+  def self.find_by_name(name)
+    query = <<-SQL
+      SELECT * FROM songs WHERE name = ? LIMIT 1
+    SQL
+    row = DB[:conn].execute(query, name)[0]
+    self.new_from_db(row)
+  end
+
 end
